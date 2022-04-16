4c16729cc962319e1b5a4c133f6b033db7da5570
sahatreact <sahatreactjs@gmail.com>
2022-04-17 00:42:35 +0700

    advanced props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,15 +1,17 @@
 type GreetProps = {
   name: string;
-  messageCount: number;
+  messageCount?: number;
   isLoggedIn: boolean;
 };
 
 export const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props;
+
   return (
     <div>
       <h2>
         {props.isLoggedIn
-          ? `Welcome ${props.name}! you have ${props.messageCount} unread memssage`
+          ? `Welcome ${props.name}! you have ${messageCount} unread memssage`
           : `Welcome guest`}
       </h2>
     </div>
