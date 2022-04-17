29121a4e31427ad8c02e18d977a11fde94b7ac56
sahatreact <sahatreactjs@gmail.com>
2022-04-17 22:47:11 +0700

    Class Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { UserContextProvider } from "./components/context/UserContext";
 import { User } from "./components/context/User";
 import "./App.css";
 import { MutableRef } from "./components/ref/MutableRef";
+import { Counter } from "./components/class/Counter";
 
 function App() {
   return (
     <div className="App">
-      {/* <UserContextProvider>
-        <User />
-      </UserContextProvider> */}
-      <MutableRef />
+      <Counter message="The count value is" />
     </div>
   );
 }
