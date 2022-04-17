8940a246533dc3ff2770aff6193729d8191c7008
sahatreact <sahatreactjs@gmail.com>
2022-04-17 11:51:01 +0700

    useContext Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,13 @@
+import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { Box } from "./components/context/Box";
 import "./App.css";
-import { Button } from "./components/Button";
-import { Container } from "./components/Container";
-import { Greet } from "./components/Greet";
-import { Heading } from "./components/Heading";
-import { Input } from "./components/Input";
-import { Oscar } from "./components/Oscar";
-import { Person } from "./components/Person";
-import { PersonList } from "./components/PersonList";
-import { Status } from "./components/Status";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Pricess",
-      last: "Diana",
-    },
-  ];
-
   return (
     <div className="App">
-      {/* <Greet name={"tafli"} messageCount={20} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList name={nameList} /> */}
-      {/* <Status status="loading" />
-      <Heading>Placeholder text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicpario</Heading>
-      </Oscar>
-      <Greet name={"tafli"} isLoggedIn={false} /> */}
-      {/* <Button
-        handleClick={(event, id) => {
-          console.log("Button Clicked", event, id);
-        }}
-      />
-      <Input value="" handleChange={(event) => console.log(event)} /> */}
-      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
